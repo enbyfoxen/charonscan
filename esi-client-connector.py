be75533618002ade820a8316b17ac183ac7584f1
Luke-Antra <h.luke1999@gmail.com>
2019-09-24 22:11:12 +0200

    wip of esiclient-connector

---
+++ esi-client-connector.py
@@ -0,0 +1,27 @@
+import requests_unixsocket
+import json
+import urllib.parse
+with open('localscan.json') as f:
+    scan = json.load(f)
+    f.close
+
+class EsiClient:
+    def __init__(self, socket_path):
+        self.socket_path = urllib.parse.quote(socket_path, safe='')
+        self.session = requests_unixsocket.Session()
+        try:
+            self.session.post('http+unix://' + self.socket_path)
+        except requests_unixsocket.requests.exceptions.RequestException as e:
+            print(e)
+            print('Could not connect to esiclient on defined socket. Please check socket path, if esiclient is running, and possibly permissions.')
+            exit(1)
+
+        
+
+    def submit(self, local_json):
+        r = self.session.post('http+unix://' + self.socket_path + '/local', json=local_json)
+        return r.json()
+
+if __name__ == "__main__":
+    client = EsiClient('/tmp/server.sock')
+    print(client.submit((scan)))
